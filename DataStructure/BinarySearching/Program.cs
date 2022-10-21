a1f1f8c5f91d5239365bb7fb377b29d762cd02fa
Prakash <prakash.sf3972>
2022-10-21 19:28:16 +0530

    FT-3154 Added Searching Algorithm Files

---
+++ DataStructure/BinarySearching/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace BinarySearching;
+class Program{
+    public static void Main(string[] args)
+    {
+        System.Console.WriteLine("Enter Size Of an Array : ");
+        int size=int.Parse(Console.ReadLine());
+        int[] elementsBox=new int[size];
+        System.Console.WriteLine("Enter a Numbers : ");
+        for(int i=0;i<size;i++)
+        {
+            elementsBox[i]=int.Parse(Console.ReadLine());
+        }
+        //int small=elementsBox[0];
+        int temp=0;
+        for(int i=0;i<size;i++)
+        {
+            for(int j=i+1;j<size;j++)
+            {
+                if(elementsBox[i]>elementsBox[j])
+                {
+                    temp=elementsBox[i];
+                    elementsBox[i]=elementsBox[j];
+                    elementsBox[j]=temp;
+                }
+            }
+        }
+       System.Console.WriteLine("SORTED ELEMENT ARE : ");
+       for(int i=0;i<size;i++)
+       {
+        System.Console.WriteLine(elementsBox[i]);
+       }
+       //Binary Searching
+       int startingIndex=0;
+       int endingIndex=size-1;
+       System.Console.WriteLine("Enter a Number to Search in Index : " );
+       int search=int.Parse(Console.ReadLine());
+       int mid=(startingIndex+endingIndex/2);
+       string condition="true";
+       while(startingIndex<endingIndex)
+       {
+            if(search==elementsBox[mid])
+            {
+                System.Console.WriteLine("The Searching Element is :"+mid);
+                condition="false";
+                break;
+            }
+            else if(search < elementsBox[mid])
+            {
+                endingIndex=mid-1;
+            }
+            else
+            {
+                startingIndex=mid+1;
+            }
+       }
+       if(condition=="true")
+       {
+        System.Console.WriteLine("Search Element is Not Present "+search);
+       }
+
+    }
+}
---
+++ DataStructure/LinearSearching/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace LinearSearching;
+class Program{
+    public static void Main(string[] args)
+    {
+        int[] numbers=new int[10];
+        System.Console.WriteLine("Enter a Number: ");
+        for(int i=0;i<numbers.Length;i++)
+        {
+            numbers[i]=int.Parse(Console.ReadLine());
+        }
+        System.Console.WriteLine("Enter a Number to find in this Array : ");
+        int findNumber=int.Parse(Console.ReadLine());
+        int temp=0;
+        for(int j=0;j<numbers.Length;j++)
+        {
+            if(numbers[j]==findNumber)
+            {
+                System.Console.WriteLine("Number is Present in the IndexList is :"+j);
+                temp=1;
+                break;
+            }
+        }
+        if(temp==0)
+        {
+            System.Console.WriteLine("Number is Not Present in the List");
+        }
+    }
+}
